438e22d58f7fdc0a550f3227f2ffa032dface083
cutebaby <lovemath0630@gmail.com>
2017-10-03 16:18:26 +0800

    first commit

---
+++ src/com/cutebaby/Person.java
@@ -0,0 +1,15 @@
+package com.cutebaby;
+
+public class Person {
+	public void sayHello(){
+		System.out.println("Hello!");
+	}
+		public static void main(String[] args) {
+			System.out.println("Hello world");
+			
+			Person p = new Person();
+			p.sayHello();
+		
+		}
+
+}
