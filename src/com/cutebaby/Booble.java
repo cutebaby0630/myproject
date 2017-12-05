e143bb5ff4719590d7b838a06ce3db97478de8bf
cutebaby <lovemath0630@gamil.com>
2017-12-05 14:09:22 +0800

    Booble

--- src/com/cutebaby/Booble.java
+++ src/com/cutebaby/Booble.java
@@ -1,25 +1,24 @@
 package com.cutebaby;
 
-public class Replase {
+public class Booble {
 
 	public static void main(String[] args) {
-		// {51,23,6,65,15}由小到大排序
-		int n[] = { 51, 23, 6, 65, 15 };
-
+		int n[] = { 51, 23, 6, 65, 15 ,89,100,1235,358};
 		for (int i = 0; i < n.length - 1; i++) {
-			// -1→最後一個最後不用比
-			for (int j = i + 1; j < n.length; j++) {
+			for (int j = (i + 1); j < n.length; j++) {
+
 				if (n[i] > n[j]) {
 					int tmp = n[i];
 					n[i] = n[j];
 					n[j] = tmp;
 				}
+
 			}
+
 		}
-		for (int num : n) {
+		for(int num:n){
 			System.out.print(num + " ");
 		}
-
 	}
 
 }
