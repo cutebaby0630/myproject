c0cf4d5564066df687a13c5d79b6902f65add594
cutebaby <lovemath0630@gamil.com>
2017-12-19 15:22:24 +0800

    finish

--- src/com/ibm/NineLazy.java
+++ src/com/ibm/NineLazy.java
@@ -5,13 +5,21 @@ public class NineLazy {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 9; i++) {
 			for (int n = 2; n <= 9; n++) {
-				if(i*n<10){
-					System.out.print(n + "x" + i + "="+" " + (i * n) + "\t");
-				}else{
-					System.out.print(n + "x" + i + "=" + (i * n) + "\t");
+				if(i>3&&i<9){
+					System.out.print(" " + "..." + "\t");
+					}else{
+						if((i * n)<10){
+							System.out.print(n + "x" + i + "=" + " "+(i * n) + "\t");
+						}else{
+							System.out.print(n + "x" + i + "=" + (i * n) + "\t");
+						}
+						
+						
+					}
+					
 				}
-				
-			}
+			
+			
 			System.out.println();
 	}
 
