5b6938b7f08a8a8b7483a4fb9d659dd9d906d196
cutebaby <lovemath0630@gmail.com>
2018-01-02 14:45:23 +0800

    繼承

--- src/com/cutebaby/oo/GraduateStudent.java
+++ src/com/cutebaby/oo/GraduateStudent.java
@@ -1,11 +1,19 @@
 package com.cutebaby.oo;
 
 public class GraduateStudent extends Students {
-//	如果沒有extends，都會繼承 java.lang.Object	
-//	extends 代表繼承  ，只能繼承一個→GraduateStudent 繼承 Students
-//　	剛開始裡面會有空的建構子，但繼承物裡面沒有空的建構子，所以會出錯
-//  →Students裡面要加一個空的建構子
-	
-	
+	// 如果沒有extends，都會繼承 java.lang.Object
+	// extends 代表繼承 ，只能繼承一個→GraduateStudent 繼承 Students
+	// 剛開始裡面會有空的建構子，但繼承物裡面沒有空的建構子，所以會出錯
+	// →Students裡面要加一個空的建構子
+
 	int thiese;
+
+	public GraduateStudent() {
+		
+	}
+	public void print(){
+		System.out.println(pass);
+		System.out.println(english + "\t" + chinese + "\t" + math + "\t");
+
+	}
 }
--- src/com/cutebaby/oo/Students.java
+++ src/com/cutebaby/oo/Students.java
@@ -7,6 +7,10 @@ public class Students {
 	String name;
 	// 區域變數 ：chinese; english; math;
 	static int pass = 60;
+	// 屬性前的沒加，別的package找不到
+	// 屬性前加private，只有當下的class看得到
+	// 屬性前加public，每一個都存取的到
+	// 父類別屬性前加protected，只能給子類別用&同package用
 
 	// 加了static就是class變數
 	public Students(String name, int english, int chinese, int math) {
@@ -15,8 +19,9 @@ public class Students {
 		this.chinese = chinese;
 		this.math = math;
 	}
-	public Students(){
-		
+
+	public Students() {
+
 	}
 
 	public Students(String name) {
@@ -24,16 +29,17 @@ public class Students {
 		// this強調name為屬性變數，並不是區域變數
 	}
 
-	public void print() {
+	 public void print() {
 		// void 代表執行後不回傳
 		System.out.println(pass);
 		System.out.println(english + "\t" + chinese + "\t" + math + "\t");
 
 	}
+
 	public static void method() {
-		System.out.println("hello");		
-//		static只能抓到static上的東西
-//		EX.後面可以放pass不能放name
+		System.out.println("hello");
+		// static只能抓到static上的東西
+		// EX.後面可以放pass不能放name
 	}
 
 	public int getAverage() {
