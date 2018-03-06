8b55f30f11076a49c84d957bfa8cf93ee009b42a
cutebaby0630 <lovemath0630@gmail.com>
2018-03-06 16:29:14 +0800

    Invoice

---
+++ src/com/java2/object/First.java
@@ -0,0 +1,25 @@
+package com.java2.object;
+
+public class First extends Invoice {
+	
+	
+	public void print(){
+		System.out.println("win the first price");
+			}
+	
+	
+	private int Same(){
+		int e = r.nextInt(10);
+		int f = r.nextInt(10);
+		int g = r.nextInt(10);
+		int h = r.nextInt(10);
+		int i = r.nextInt(10);
+		int j = r.nextInt(10);
+		int k = r.nextInt(10);
+		int l = r.nextInt(10);
+		
+		
+		
+	}
+	
+}
