7b87d72db54628b5a7bf625c1e946a4541f1ee8b
cutebaby <lovemath0630@gamil.com>
2018-03-06 15:54:55 +0800

    Invoice

---
+++ src/com/java2/object/Invoice.java
@@ -0,0 +1,17 @@
+package com.java2.object;
+
+import java.util.Random;
+
+public class Invoice {
+	Random r = new Random();
+	
+	int a = r.nextInt(10);
+	int b = r.nextInt(10);
+	int c = r.nextInt(10);
+	public Invoice(int x, int y , int z){
+		}
+	public void print() {
+		System.out.println("win 200");
+	}
+	
+}
