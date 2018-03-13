49462ee4c883e32df38a6c0f643c6b3db71e7d3f
cutebaby0630 <lovemath0630@gmail.com>
2018-03-13 16:14:00 +0800

    Override

--- src/com/java2/object/TicketThirty.java
+++ src/com/java2/object/TicketThirty.java
@@ -2,11 +2,12 @@ package com.java2.object;
 
 
 public class TicketThirty extends Ticket {
-	private float discount=0.15f;
+	private float discount=0.5f;
 	String rt;
 	private int rm = (int)((amount*30)*(1-discount));
 	int s1;
 	int s2;
+	String dt;
 	public TicketThirty(String date,String time,String start,String finish,String rt,int rm){
 		super(date,time,start,finish,rm);
 		this.rt=rt;
@@ -15,6 +16,7 @@ public class TicketThirty extends Ticket {
 	@Override
 	public void print(){
 		System.out.println(date+"\t"+time+"\t"+start+"\t"+finish+"\t"+rt+"\t"+rm);
+		System.out.print(dt);
 	
 		
 	} 
