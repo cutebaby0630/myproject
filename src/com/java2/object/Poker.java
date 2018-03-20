418a1b0e5263e366c3aa5157a66c8f3788e0365a
cutebaby <lovemath0630@gmail.com>
2018-03-20 16:27:42 +0800

    poker

---
+++ src/com/java2/object/Poker.java
@@ -0,0 +1,21 @@
+package com.java2.object;
+
+import java.util.Random;
+
+public class Poker {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int [] cards = new int [52];
+		String flowers = "♠♥♦♣";
+		for(int i=0;i<cards.length;i++){
+			cards[i]=i;
+			System.out.println((i%13)+1 + "" +(flowers.charAt(i/13)));
+		}
+		for(int i=0;i<cards.length;i++){
+			
+		}
+		
+	}
+
+}
