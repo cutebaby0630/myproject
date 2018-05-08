81c2aadb8844ec0692bd7ee36b1f76e91a78ae85
cutebaby0630 <cutebaby0630@gmail.com>
2018-05-08 15:07:28 +0800

    Stepik-List

---
+++ src/com/cutebaby/stepik/ArrayList2.java
@@ -0,0 +1,10 @@
+package com.cutebaby.stepik;
+
+public class ArrayList2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/cutebaby/stepik/ArraytList1.java
@@ -0,0 +1,18 @@
+package com.cutebaby.stepik;
+
+import java.util.ArrayList;
+
+public class ArraytList1 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> a = new ArrayList<>();
+		a.add(2);
+		a.add(0);
+		a.add(1);
+		a.add(7);
+		for(int  name : a){
+			System.out.print(name);
+		}
+	}
+
+}
