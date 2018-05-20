86ae2e781027b4f911c6e6c1a5aa0d24908ba06c
cutebaby0630 <lovemath0630@gmail.com>
2018-05-20 21:35:27 +0800

    20180519stepik

--- src/com/cutebaby/stepik/ArrayList4.java
+++ src/com/cutebaby/stepik/ArrayList4.java
@@ -11,11 +11,13 @@ public class ArrayList4 {
 	public static void main(String[] args) {
 		// List<Integer> a = new ArrayList<>();
 		// Scanner scanner = new Scanner(System.in);
-		String line = "1 2 3 4 5 6";
+		String line = "1 2 3 4 5 6 7";
+		
 		String[] token = line.split(" ");
-		for (int i = token.length; i >= 0; i--) {
-			
-				System.out.println(token[i]);
+		for (int i =token.length;i >0;i--) {
+			if(i%2==0) {
+			System.out.print(i+" ");
+			}
 			
 		}
 		// System.out.println(line);
