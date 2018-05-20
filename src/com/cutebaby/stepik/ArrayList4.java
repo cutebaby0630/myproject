240fdf27b23c0c151358599ebc052d2503a4b993
cutebaby0630 <lovemath0630@gmail.com>
2018-05-20 21:43:07 +0800

    20180519stepik-1

--- src/com/cutebaby/stepik/ArrayList4.java
+++ src/com/cutebaby/stepik/ArrayList4.java
@@ -3,6 +3,7 @@ package com.cutebaby.stepik;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Spliterator;
@@ -22,6 +23,30 @@ public class ArrayList4 {
 		}
 		// System.out.println(line);
 
+		
+		List<Integer> list1 = new ArrayList<>();
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+
+		List<Integer> list2 = new LinkedList<>();
+		list2.add(1);
+		list2.add(2);
+		list2.add(3);
+
+		List<Integer> list3 = new ArrayList<>();
+		list3.add(3);
+		list3.add(1);
+		list3.add(2);
+		
+		System.out.println(list3.equals(list2));
+		System.out.println(list1.equals(list1));
+		System.out.println (list1.equals(list2));
+		System.out.println(list1.equals(list3));
+		 
+		
+		 
+		 
 	}
 
 }
