a98d6571d5232b3ec32336e7a03aabbc7d69e61b
cutebaby0630 <lovemath0630@gmail.com>
2018-06-05 15:41:51 +0800

    Vending machine

--- src/com/java2/oo/VendingMain.java
+++ src/com/java2/oo/VendingMain.java
@@ -1,13 +1,40 @@
 package com.java2.oo;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VendingMain {
 
 	public static void main(String[] args) {
+		VendingMachine vm = new VendingMachine();
+		FileReader fr ;
+		try {
+			fr = new FileReader("vending.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String line = br.readLine();
+			String[] tokens = line.split(",");
+			int count = Integer.parseInt(tokens[0]);
+			for(int i=0;i<count;i++){
+				String name = tokens[i*2+1];
+				int price = Integer.parseInt(tokens[i*2+2]);
+				vm.drinks.add(new Drink(i+1, name, price));
+			}
+			vm.start();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		List<Drink> drinks = new ArrayList<>();
+		
+		
+		
 		
 	}
 
