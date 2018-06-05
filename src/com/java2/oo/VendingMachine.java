d3f789690cdcb43f4b13075eed76208022f14625
cutebaby0630 <lovemath0630@gmail.com>
2018-06-05 14:54:19 +0800

    Drink

---
+++ src/com/java2/oo/VendingMachine.java
@@ -0,0 +1,20 @@
+package com.java2.oo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class VendingMachine {
+	int total = 0;
+	List<Drink> drinks = new ArrayList<>();
+
+	public void start(){
+		System.out.println("please insert coins.");
+		for(int i=0; i<drinks.size();i++){
+			
+		}
+		Scanner scanner = new Scanner(System.in);
+		
+	}
+	
+}
