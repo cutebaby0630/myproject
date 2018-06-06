c3bf273b56c48cfbe41e3f492afebad8b4777dbc
cutebaby0630 <lovemath0630@gmail.com>
2018-06-06 21:50:05 +0800

    maze1

--- src/com/java2/mazegame/MazeMain.java
+++ src/com/java2/mazegame/MazeMain.java
@@ -13,19 +13,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class MazeMain {
 	public MazeMain(){
 		try {
+//			地圖大小
 			FileReader fr = new FileReader("maze.txt");
 			BufferedReader br = new BufferedReader(fr);
 			String line = br.readLine();
 			String[] map = line.split(",");
 			int col = Integer.parseInt(map[0]);
 			int row = Integer.parseInt(map[1]);
+//			陷阱個數
 			line= br.readLine();
 			int trapCount = Integer.parseInt(line);
 			Maze m = new Maze(row, col, trapCount);
+			Random r = new Random();
+			Set<String> set = new TreeSet<>();
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
